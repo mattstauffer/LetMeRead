1757a837232778afb9da067ddb37deec6def1554
Matt Stauffer <matt@mattstaufferdesign.com>
2012-11-09 11:18:22 -0600

    Initial commit.

---
+++ LetMeRead.js
@@ -0,0 +1,15 @@
+function(){ 
+	// http://stackoverflow.com/questions/566203/changing-css-values-with-javascript
+		function css(selector, property, value) {
+		for (var i=0; i<document.styleSheets.length;i++) {//Loop through all styles
+			//Try add rule
+			try { document.styleSheets[i].insertRule(selector+ ' {'+property+':'+value+'}', document.styleSheets[i].cssRules.length);
+			} catch(err) {try { document.styleSheets[i].addRule(selector, property+':'+value);} catch(err) {}}//IE
+		}
+	}  
+	// @todo: Crap, what about if it's set on the rest of the elements? body *? This is more complicated than I thought.
+	// @todo: Haven't even tested this, but need to get back to work. So.. test.
+	css('body', 'font-size', '14px!important');
+	css('body', 'color', '#333!important');
+	css('body', 'background', '#fff!important');
+};
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-let-me-read
+LetMeRead
 ===========
 
 A mini bookmarklet that forces the base body font of the current web site to be a dark, readable color.
+
+@todo: Make a linkable version of LetMeRead.js that is just this:
+
+    javascript: (function() { // Script here } )();
